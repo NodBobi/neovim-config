8c1d6f83bd0970a73fa0a00c855772db1d451888
Luukas <luukasniemin@gmail.com>
2024-03-29 21:39:48 +0200

    mid commit.

--- init.lua
+++ init.lua
@@ -11,7 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = " "
+vim.g.mapleader = " ";
+vim.g.maplocalleader = ","
 
-require("lazy").setup("plugins")
+require("lazy").setup("plugins");;
 require("vim-options")
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,18 +1,17 @@
-vim.cmd("set number")
+vim.cmd("set relativenumber")
 vim.opt.foldlevelstart = 99
-vim.cmd("color catppuccin-mocha")
+vim.cmd("color gruvbox-material")
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.g.mapleader = " "
-vim.g.maplocalleader = ","
 vim.opt.conceallevel = 3
 
 -- Set custom commands to run on neovim startup
 
 -- Set custom keymap bindings
 -- Open this config directory
-vim.keymap.set("n", "<leader>config", ":edit /home/luukas/.config/nvim/init.lua | :cd ~/.config/nvim <CR>", {})
+vim.keymap.set("n", "<leader>config", ":edit /home/luukas/.config/nvim/init.lua | :cd ~/.config/nvim<CR>", {})
 -- Open a file finder in telescope
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>")
 -- Open a live grep on telescope
@@ -22,13 +21,18 @@ vim.keymap.set("n", "<leader>term", ":split | :terminal<CR>")
 -- Open a (vertical) terminal in a new buffer
 vim.keymap.set("n", "<leader>vsh", ":vsplit | :terminal<CR>")
 -- Save all changed files and then open a shell in a new buffer
-vim.keymap.set("n", "<leader>wsh", ":wa | :split | :terminal<CR>")
+vim.keymap.set("n", "<leader>wt", ":wa | :split | :terminal<CR>")
 -- Open a neovim cheatsheet
 vim.keymap.set("n", "<leader>csh", ":Cheatsheet<CR>")
 -- Open the ChatGPT window
 vim.keymap.set("n", "<leader>gpt", ":ChatGPT<CR>");
 -- Open the todos menu
 vim.keymap.set("n", "<leader>todo", ":TodoTelescope<CR>");
+-- Go to the end of the current line:
+vim.keymap.set("n", "<leader>el", "$");
+-- Go to the beginning of the line
+vim.keymap.set("n", "<leader>bl", "0");
+
 
 -- Custom functionality
 vim.keymap.set("n", "<leader>raw", function()
