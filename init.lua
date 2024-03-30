3f7b234fb32e314e2d1f724148de87c818c6e1de
Luukas <luukasniemin@gmail.com>
2024-03-31 00:20:45 +0200

    Added command to jump backwards on luasnip

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 -- Install lazy.nvim, the nvim package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -16,8 +16,7 @@ vim.opt.rtp:prepend(lazypath)
 require("config.keymaps")
 
 -- Start the lazy plugin manager
-require("lazy").setup("plugins");;
+require("lazy").setup("plugins")
 
 -- Import the options file
 require("config.options")
-
